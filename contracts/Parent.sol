0ee6183de04368123d28e29d6dc4c8f6a98c0847
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-08-23 18:43:01 +1000

    Update tests

---
+++ contracts/Parent.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.24;
+
+import "./ERC20.sol";
+
+contract Parent {
+
+    ERC20 private token;
+
+    constructor (address erc20) public {
+        token = ERC20(erc20);
+    }
+
+    function transferERC20(address to, uint256 value) public {
+        token.transfer(to, value);
+    }
+}
